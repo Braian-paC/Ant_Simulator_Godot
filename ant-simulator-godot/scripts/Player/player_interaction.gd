f96ac5157447ea35c72d711cf7b6d353586703db
Braian-paC <braianbbg4@gmail.com>
2026-06-24 22:02:07 -0300

    Pick up the food in the world and carry into the Anthill (bug present in food sprite)

--- ant-simulator-godot/scripts/Player/player_interaction.gd
+++ ant-simulator-godot/scripts/Player/player_interaction.gd
@@ -1,23 +1,22 @@
 extends Node
 
-var food_node = null
 @export var food_name = "StaticBody2D_Food"
 
 func handle(player):
 	if Input.is_action_just_pressed("InteractItem"):
-		if food_node:
+		if GameManager.food_node:
 			var pos = player.global_position
-			player.get_parent().add_child(food_node)
-			food_node.global_position = pos
-			food_node = null
+			player.get_parent().add_child(GameManager.food_node)
+			GameManager.food_node.global_position = pos
+			GameManager.food_node = null
 
 		else:
 			for i in range(player.get_slide_collision_count()):
 				var col = player.get_slide_collision(i).get_collider()
 
 				if col.name == food_name:
-					food_node = col
-					food_node.get_parent().remove_child(food_node)
+					GameManager.food_node = col
+					GameManager.food_node.get_parent().remove_child(GameManager.food_node)
 
 	if Input.is_action_just_pressed("ExitGame"):
 		get_tree().quit()
